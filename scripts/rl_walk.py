ad7a7f7f146fc623d486a35a02c6e9b19b43512a
apirrone <awanegenebayo@gmail.com>
2024-07-23 13:02:51 +0200

    update

--- scripts/rl_walk.py
+++ scripts/rl_walk.py
@@ -43,10 +43,10 @@ class RLWalk:
 
         self.angularVelocityScale = 0.25
         self.dof_pos_scale = 1.0
-        self.dof_vel_scale = 0.005
-        # self.dof_vel_scale = 0.05
+        self.dof_vel_scale = 0.05
+        # self.dof_vel_scale = 0.005
         # self.action_clip = (-1, 1)
-        self.action_clip = (-0.1, 0.1)
+        self.action_clip = (-0.5, 0.5)
         self.obs_clip = (-5, 5)
         self.zero_yaw = None
 
