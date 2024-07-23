96b5c27b8992f4c4e9662fe5de39047ea4ae67c9
apirrone <awanegenebayo@gmail.com>
2024-07-23 14:20:13 +0200

    update

--- scripts/rl_walk.py
+++ scripts/rl_walk.py
@@ -195,7 +195,7 @@ class RLWalk:
 
     def start(self):
         self.hwi.turn_on()
-        pid = [500, 0, 100]
+        pid = [400, 0, 100]
         # pid = [100, 0, 50]
         self.hwi.set_pid_all(pid)
 
