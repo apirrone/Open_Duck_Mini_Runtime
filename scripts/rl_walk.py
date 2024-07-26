5db383fb672ca232574760cfa9844046c6bfa776
apirrone <awanegenebayo@gmail.com>
2024-07-26 14:00:43 +0200

    update

--- scripts/rl_walk.py
+++ scripts/rl_walk.py
@@ -57,7 +57,7 @@ class RLWalk:
         self.angularVelocityScale = 0.25
         self.dof_pos_scale = 1.0
         # self.dof_vel_scale = 0.05
-        self.dof_vel_scale = 0.01
+        self.dof_vel_scale = 0.05
         self.action_clip = (-1, 1)
         self.obs_clip = (-5, 5)
         self.zero_yaw = None
@@ -173,7 +173,6 @@ class RLWalk:
                 action = self.action_filter.get_filtered_action()
 
                 self.prev_action = action.copy()  # here ? # Maybe here
-                self.prev_action *= 2
                 action = self.isaac_init_pos + action
 
                 robot_action = isaac_to_mujoco(action)
