64638409de0fd3e9d72f469987dd267b419020ae
apirrone <awanegenebayo@gmail.com>
2024-07-26 12:27:18 +0200

    update

--- scripts/rl_walk.py
+++ scripts/rl_walk.py
@@ -154,9 +154,9 @@ class RLWalk:
         robot_computed_obs = []
         try:
             print("Starting")
+            commands = [0.1, 0.0, 0.0]
             while True:
                 start = time.time()
-                commands = [0.0, 0.0, 0.0]
                 obs = self.get_obs(commands)
                 if obs is None:
                     break
