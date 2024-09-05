9338580faffb0638a7c4e2d96c3e7a721fb4a675
apirrone <awanegenebayo@gmail.com>
2024-09-05 17:14:28 +0200

    control server test

--- scripts/client_test.py
+++ scripts/client_test.py
@@ -1,4 +1,5 @@
 import socket
+import json
 
 
 def client_program():
@@ -8,15 +9,11 @@ def client_program():
     client_socket = socket.socket()  # instantiate
     client_socket.connect((host, port))  # connect to the server
 
-    message = input(" -> ")  # take input
-
     while message.lower().strip() != "bye":
-        client_socket.send(message.encode())  # send message
         data = client_socket.recv(1024).decode()  # receive response
+        data = json.loads(data)
 
         print("Received from server: " + data)  # show in terminal
-
-        message = input(" -> ")  # again take input
 
     client_socket.close()  # close the connection
 
--- scripts/control_server.py
+++ scripts/control_server.py
@@ -1,31 +1,98 @@
 import socket
+import json
+import numpy as np
+import pygame
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-c",
+    "--controller",
+    action="store_true",
+    default=False,
+    help="if not set, uses keyboard",
+)
+args = parser.parse_args()
+
+X_RANGE = [0, 0.3]
+Y_RANGE = [0, 0.0]
+YAW_RANGE = [-0.7, 0.7]
+
+if args.controller:
+    pygame.init()
+    _p1 = pygame.joystick.Joystick(0)
+    _p1.init()
+    print(f"Loaded joystick with {_p1.get_numaxes()} axes.")
+else:
+    pygame.init()
+    # open a blank pygame window
+    screen = pygame.display.set_mode((100, 100))
+    pygame.display.set_caption("Press arrow keys to move robot")
 
 
-def server_program():
-    # get the hostname
-    host = "localhost"
-    port = 1234  # initiate port no above 1024
+def get_command():
+    commands = [0, 0, 0]
+    if args.controller:
+        for event in pygame.event.get():
+            lin_vel_x = -1 * _p1.get_axis(1)
+            lin_vel_y = -1 * _p1.get_axis(3)
+            ang_vel = -1 * _p1.get_axis(0)
+            if lin_vel_x >= 0:
+                lin_vel_x *= np.abs(X_RANGE[1])
+            else:
+                lin_vel_x *= np.abs(X_RANGE[0])
 
-    server_socket = socket.socket()  # get instance
-    # look closely. The bind() function takes tuple as argument
-    server_socket.bind((host, port))  # bind host address and port together
+            if lin_vel_y >= 0:
+                lin_vel_y *= np.abs(Y_RANGE[1])
+            else:
+                lin_vel_y *= np.abs(Y_RANGE[0])
 
-    # configure how many client the server can listen simultaneously
-    server_socket.listen(1)
-    conn, address = server_socket.accept()  # accept new connection
-    print("Connection from: " + str(address))
-    while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = conn.recv(1024).decode()
-        if not data:
-            # if data is not received break
-            break
-        print("from connected user: " + str(data))
-        data = input(" -> ")
-        conn.send(data.encode())  # send data to the client
+            if ang_vel >= 0:
+                ang_vel *= np.abs(YAW_RANGE[1])
+            else:
+                ang_vel *= np.abs(YAW_RANGE[0])
 
-    conn.close()  # close the connection
+            commands[0] = lin_vel_x
+            commands[1] = lin_vel_y
+            commands[2] = ang_vel
+    else:
+        keys = pygame.key.get_pressed()
+        lin_vel_x = 0
+        lin_vel_y = 0
+        ang_vel = 0
+        if keys[pygame.K_z]:
+            lin_vel_x = X_RANGE[1]
+        elif keys[pygame.K_s]:
+            lin_vel_x = X_RANGE[0]
+
+        if keys[pygame.K_q]:
+            ang_vel = YAW_RANGE[1]
+        elif keys[pygame.K_d]:
+            ang_vel = YAW_RANGE[0]
+
+        commands[0] = lin_vel_x
+        commands[1] = lin_vel_y
+        commands[2] = ang_vel
+
+        pygame.event.pump()  # process event queue
+
+    print(commands)
+    return commands
 
 
-if __name__ == "__main__":
-    server_program()
+host = "0.0.0.0"
+port = 1234
+
+server_socket = socket.socket()
+server_socket.bind((host, port))
+
+server_socket.listen(1)
+conn, address = server_socket.accept()  # accept new connection
+print("Connection from: " + str(address))
+
+while True:
+    data = get_command()
+    data = json.dumps(data)
+    conn.send(data.encode())  # send data to the client
+
+conn.close()  # close the connection
