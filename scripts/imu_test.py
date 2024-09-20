07ae357e4722e897bffe1b775f2bed39f6841723
apirrone <awanegenebayo@gmail.com>
2024-09-20 15:48:38 +0200

    update

--- scripts/imu_test.py
+++ scripts/imu_test.py
@@ -7,8 +7,12 @@ uart = serial.Serial("/dev/ttyS0")  # , baudrate=115200)
 imu = adafruit_bno055.BNO055_UART(uart)
 
 while True:
-    raw_orientation = imu.quaternion  # quat
-    euler = R.from_quat(raw_orientation).as_euler("xyz")
+    try:
+        raw_orientation = imu.quaternion  # quat
+        euler = R.from_quat(raw_orientation).as_euler("xyz")
+        print(euler)
+    except Exception as e:
+        print(e)
+        continue
 
-    print(euler)
     time.sleep(1 / 30)
