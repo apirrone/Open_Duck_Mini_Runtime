c6fb6131efd3fef9e3cddddb775ff9350c300d98
apirrone <awanegenebayo@gmail.com>
2024-09-21 19:14:42 +0200

    bluetooth commands :)

--- scripts/rl_walk.py
+++ scripts/rl_walk.py
@@ -244,13 +244,13 @@ class RLWalk:
 
                 i += 1
                 took = time.time() - start
-                # print(
-                #     "FPS",
-                #     np.around(1 / took, 3),
-                #     "-- target",
-                #     self.control_freq,
-                # )
-                # print("===")
+                print(
+                    "FPS",
+                    np.around(1 / took, 3),
+                    "-- target",
+                    self.control_freq,
+                )
+                print("===")
                 time.sleep((max(1 / self.control_freq - took, 0)))
         except KeyboardInterrupt:
             pass
