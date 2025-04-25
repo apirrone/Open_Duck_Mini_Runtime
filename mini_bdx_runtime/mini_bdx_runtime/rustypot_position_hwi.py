32e1d1c3ac869512a0f701243cbfa3f33b65aee3
apirrone <awanegenebayo@gmail.com>
2025-04-25 17:36:50 +0200

    doesn't make sense to keep the usb_port argument in HWI

--- mini_bdx_runtime/mini_bdx_runtime/rustypot_position_hwi.py
+++ mini_bdx_runtime/mini_bdx_runtime/rustypot_position_hwi.py
@@ -6,7 +6,7 @@ from mini_bdx_runtime.duck_config import DuckConfig
 
 
 class HWI:
-    def __init__(self, duck_config: DuckConfig, usb_port: str = None):
+    def __init__(self, duck_config: DuckConfig):
 
         self.duck_config = duck_config
 
@@ -74,8 +74,7 @@ class HWI:
         self.kds = np.ones(len(self.joints)) * 0  # default kd
         self.low_torque_kps = np.ones(len(self.joints)) * 2
 
-        usb_port = usb_port if usb_port else self.duck_config.serial_port
-        self.io = rustypot.feetech(usb_port, 1000000)
+        self.io = rustypot.feetech(self.duck_config.serial_port, 1000000)
 
     def set_kps(self, kps):
         self.kps = kps
