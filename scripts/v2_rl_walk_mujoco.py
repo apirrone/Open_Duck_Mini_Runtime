abf42d3c127269df70b9b8e054219e4155c310cd
apirrone <awanegenebayo@gmail.com>
2025-05-05 10:25:05 +0200

    no phase ticking when no commands

--- scripts/v2_rl_walk_mujoco.py
+++ scripts/v2_rl_walk_mujoco.py
@@ -254,9 +254,10 @@ class RLWalk:
                 if obs is None:
                     continue
 
-                self.imitation_i += 1 * (
-                    self.phase_frequency_factor + self.phase_frequency_factor_offset
-                )
+                if np.linalg.norm(self.last_commands[:3]) > 0.01:
+                    self.imitation_i += 1 * (
+                        self.phase_frequency_factor + self.phase_frequency_factor_offset
+                    )
                 self.imitation_i = self.imitation_i % self.PRM.nb_steps_in_period
                 self.imitation_phase = np.array(
                     [
